0348c25e51024c4dc601c197c7d8d612cc7df538
eugene <eugene.makarov@yongletechnologies.com>
2019-10-15 17:26:31 -0700

    Comments and header update

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-(function(){
-    smoothScroll.addElement('image-clip-container', 0, 1, 2);
-    smoothScroll.addElement('background-image2', 0, 1, 1.8);
-    smoothScroll.addElement('background-image3', 50, -1, 1.9);
-    smoothScroll.addElement('background-image4', -240, 1, 10);
-    smoothScroll.addElement('background-image5', 65, -1, 1.2);
+/**
+ * TODO
+ *
+ * - add a banner explaning that it is a copy with no functionality
+ *  - come up with text
+ *  - come up with design
+ *  - implement
+ *
+ * - create mobile view
+ *  - design mobile view (do we need to have much difference, or just make what we have look nice)
+ *      -header/footer/split view content pieces vertically
+ *  - implement
+ */
+
+(function() {
+  smoothScroll.addElement('image-clip-container', 0, 1, 2);
+  smoothScroll.addElement('background-image2', 0, 1, 1.8);
+  smoothScroll.addElement('background-image3', 50, -1, 1.9);
+  smoothScroll.addElement('background-image4', -240, 1, 10);
+  smoothScroll.addElement('background-image5', 65, -1, 1.2);
 })();
