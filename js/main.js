1036b109c552def2d383bbad42e9a6963b287e66
Evgeny <isimvol@gmail.com>
2020-09-12 17:49:44 -0700

    Scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-(function(){
-    // smoothScroll.addElement('image-clip-container', 0, 1, 2);
-    // smoothScroll.addElement('background-image2', 0, 1, 1.8);
-    // smoothScroll.addElement('background-image3', 50, -1, 1.9);
-    // smoothScroll.addElement('background-image4', -240, 1, 10);
-    // smoothScroll.addElement('background-image5', 95, -1, 1.2);
+(function () {
+  smoothScroll.addElement('image-clip-container', 0, 1, 2);
+  smoothScroll.addElement('background-image2', 0, 1, 1.8);
+  smoothScroll.addElement('background-image3', 50, -1, 1.9);
+  smoothScroll.addElement('background-image4', -240, 1, 10);
+  smoothScroll.addElement('background-image5', 95, -1, 1.2);
 })();
