5cc0be17b2be80bcc73dd89515f45f5a65897c19
Evgeny <isimvol@gmail.com>
2020-09-13 15:30:51 -0700

    Update smooth scroll to support mobile, update mobile footer in progress

--- js/main.js
+++ js/main.js
@@ -1,7 +1,63 @@
 (function () {
-  smoothScroll.addElement('image-clip-container', 0, 1, 2);
-  smoothScroll.addElement('background-image2', 0, 1, 1.8);
-  smoothScroll.addElement('background-image3', 50, -1, 1.9);
-  smoothScroll.addElement('background-image4', -240, 1, 10);
-  smoothScroll.addElement('background-image5', 95, -1, 1.2);
+  const MOBILE_SIZE = 920;
+  const images = [
+    {
+      className: 'image-clip-container',
+      initialPosition: 0,
+      direction: 1,
+      speed: 2,
+    },
+    {
+      className: 'background-image2',
+      initialPosition: 0,
+      direction: 1,
+      speed: 1.8,
+    },
+    {
+      className: 'background-image3',
+      initialPosition: 50,
+      direction: -1,
+      speed: 1.9,
+    },
+    {
+      className: 'background-image4',
+      initialPosition: -240,
+      direction: 1,
+      speed: 10,
+    },
+    {
+      className: 'background-image5',
+      initialPosition: 95,
+      direction: -1,
+      speed: 1.2,
+    },
+  ];
+  function setScrolling(images) {
+    images.forEach((image) => {
+      smoothScroll.addElement(
+        image.className,
+        image.initialPosition,
+        image.direction,
+        image.speed
+      );
+    });
+  }
+
+  init();
+
+  // reset position of images
+  function resetImagesPosition(images) {
+    images.forEach((image) => {
+      document.querySelector('.' + image.className).style.backgroundPositionY =
+        image.initialPosition;
+    });
+  }
+
+  function init() {
+    if (document.body.clientWidth > MOBILE_SIZE) {
+      setScrolling(images);
+    } else {
+      resetImagesPosition(images);
+    }
+  }
 })();
--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,57 +1,60 @@
-
 //A simple service to make an element move smooth on scroll
 // method "add"
 // arguments:
 //  newClassName - moving element selector
 //  newDirection - moving direction (1 or -1)
 //  newSpeed - moving speed
-var smoothScroll = (function(){
+var smoothScroll = (function () {
+  var elementsToMove = [];
 
-    var elementsToMove = [];
+  // var className = "";
+  // var initialPosition = 0;
+  // var direction = 1;
+  // var speed = 0;
 
-    // var className = "";
-    // var initialPosition = 0;
-    // var direction = 1;
-    // var speed = 0;
+  //Scroll throttling
+  var ticking = false;
+  var lastKnownScrollPosition = null;
+  window.addEventListener('scroll', function (e) {
+    lastKnownScrollPosition = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        moveElements(lastKnownScrollPosition);
+        ticking = false;
+      });
+      ticking = true;
+    }
+  });
 
-    //Scroll throttling
-    var ticking = false;
-    var lastKnownScrollPosition = null;
-    window.addEventListener('scroll', function(e) {
-        lastKnownScrollPosition = window.scrollY;
-        if (!ticking) {
-            window.requestAnimationFrame(function() {
-                moveElements(lastKnownScrollPosition);
-                ticking = false;
-            });
-            ticking = true;
-        }
+  function moveElements(position) {
+    elementsToMove.forEach(function (element) {
+      moveElement(
+        position,
+        element.initialPosition,
+        element.speed,
+        element.direction,
+        element.dom
+      );
     });
+  }
 
-    function moveElements(position){
-        elementsToMove.forEach(function(element){
-            moveElement(position, element.initialPosition, element.speed, element.direction, element.dom);
-        });
-    }
+  function moveElement(position, initialPosition, speed, direction, element) {
+    var percentage =
+      (position / document.body.clientHeight) * 100 * speed * direction;
+    element.style.backgroundPositionY = initialPosition + percentage + '%';
+  }
 
-    function moveElement(position, initialPosition, speed, direction, element){
-        var percentage = (position/ document.body.clientHeight * 100 * speed) * direction;
-        // element.style.top = initialPosition + percentage + '%';
-        // element.style.marginTop = initialPosition - percentage + '%';
-        element.style.backgroundPositionY = initialPosition + percentage + '%';
-    }
+  var methods = {
+    addElement: function (className, initialPosition, direction, speed) {
+      elementsToMove.push({
+        dom: document.getElementsByClassName(className)[0],
+        initialPosition: initialPosition || 0,
+        speed: speed || 0,
+        direction: direction || 1,
+      });
+    },
+    //TODO removeElement ?
+  };
 
-    var methods = {
-        addElement: function(className, initialPosition, direction, speed){
-            elementsToMove.push({
-                dom: document.getElementsByClassName(className)[0],
-                initialPosition: initialPosition || 0,
-                speed: speed || 0,
-                direction: direction || 1
-            });
-        }
-        //TODO removeElement ?
-    };
-
-    return methods;
+  return methods;
 })();
